fde876ba6c5ead1108343ab27c28e8c5a1d0a581
Jagamohan panda <pandajagamohan12@gmail.com>
2025-02-04 21:58:57 +0530

    [jp] cors error fixed

--- server/app.py
+++ server/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"/api/*": {"origins": "*"}}) 
 
 todos = []
 todo_id_counter = 1  
